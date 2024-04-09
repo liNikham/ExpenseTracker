1456218a49b03e937839151fddcc93ce87fc21e5
Nikhil <mahadiknikhil2508@gmail.com>
2024-04-09 10:01:00 +0530

    Updated build process

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,40 @@
 'use client'
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-const initial_expenses=[
+
+const initial_expenses = [
   {
-    id:'e1',
-    title:'Health Insurance',
-    amount:194.12,
-    date:new Date(2023,7,15),
+    id: 'e1',
+    title: 'Health Insurance',
+    amount: 194.12,
+    date: new Date(2023, 7, 15),
   },
   {
-    id:'e2',
-    title:'Car Insurance',
-    amount:295.34,
-    date :new Date(2023,7,16),
-
+    id: 'e2',
+    title: 'Car Insurance',
+    amount: 295.34,
+    date: new Date(2023, 7, 16),
   }
+];
 
-]
-const index=()=>{
-  const [expenses,setExpenses]=useState(initial_expenses);
-    
-  const AddExpenseHandler = (expense)=>{
-    
-    setExpenses((prevExpenses)=>{
-      return [expense,...prevExpenses];
+const Index = () => {
+  const [expenses, setExpenses] = useState(initial_expenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
     });
-  }
- return <div className="w-[50%]">
-  <NewExpense onAddExpense={AddExpenseHandler} />
-  <div className="bg-[#111] py-3 rounded-2xl " >
-  <Expenses expenses={expenses} />
- </div>
- </div>
- 
-}
-export default index;
+  };
+
+  return (
+    <div className="w-[50%]">
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <div className="bg-[#111] py-3 rounded-2xl">
+        <Expenses expenses={expenses} />
+      </div>
+    </div>
+  );
+};
+
+export default Index;
